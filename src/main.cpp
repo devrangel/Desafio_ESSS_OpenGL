d32ca10558b5dee466b6ef501c903bbda01ce6ac
Matheus Rangel <matheusrangel.dev@gmail.com>
2022-09-07 20:26:33 -0300

    add initial structure for loading texture

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,9 @@
 #include "glad/glad.h"
 #include "GLFW/glfw3.h"
 
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb/stb_image.h"
+
 #include "shaders/shader.h"
 #include "shaders/source.h"
 #include "utils.h"
@@ -56,16 +59,21 @@ int main()
 	 * Rendering stuff
 	 */
 	float vertices[] = {
-		-0.5f, -0.5f, 0.0f,
-		 0.5f, -0.5f, 0.0f,
-		 0.0f,  0.5f, 0.0f,
+		// Position			// Texture Coords
+		 0.5f,  0.5f, 0.0f, 1.0f, 1.0f, // top right
+		 0.5f, -0.5f, 0.0f, 1.0f, 0.0f, // bottom right
+		-0.5f, -0.5f, 0.0f, 0.0f, 0.0f, // bottom left
+		-0.5f,  0.5f, 0.0f, 0.0f, 1.0f  // top left
 	};
 
-	Shader shader(ShaderSource::vertexShaderSource, ShaderSource::fragmentShaderSource);
+	unsigned int indices[] = {
+		0, 1, 3, // first triangle
+		1, 2, 3  // second triangle
+	};
 
 	/*
-	* VAO e VBO
-	*/
+	 * VAO e VBO
+	 */
 	unsigned int VAO;
 	glGenVertexArrays(1, &VAO);
 	glBindVertexArray(VAO);
@@ -75,10 +83,50 @@ int main()
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	// Position
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
 
+	// Texture
+	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
+	glEnableVertexAttribArray(1);
+
+	// Indices
+	unsigned int EBO;
+	glGenBuffers(1, &EBO);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+	/*
+	 * Texture
+	 */
+	unsigned int texture;
+	glGenTextures(1, &texture);
+	glActiveTexture(GL_TEXTURE0);
+	glBindTexture(GL_TEXTURE_2D, texture);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+	int width, height, nrChannels;
+	stbi_set_flip_vertically_on_load(true);
+	
+	unsigned char* data = stbi_load("textures/heightmap.png", &width, &height, &nrChannels, 0);
+	if (data)
+	{
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
+	}
+	else
+	{
+		std::cout << "Failed to load texture" << std::endl;
+	}
+	stbi_image_free(data);
+
+	Shader shader(ShaderSource::vertexShaderSource, ShaderSource::fragmentShaderSource);
+
 	shader.useProgram();
+	shader.setUniformInt("uTexture", 0);
 
 	while (!glfwWindowShouldClose(window))
 	{
@@ -87,7 +135,7 @@ int main()
 		glClear(GL_COLOR_BUFFER_BIT);
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 
-		glDrawArrays(GL_TRIANGLES, 0, 3);
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
 		glfwSwapBuffers(window);
 
--- src/shaders/source.h
+++ src/shaders/source.h
@@ -2,18 +2,25 @@ namespace ShaderSource
 {
 	static const char* vertexShaderSource = R"(#version 330 core
 	layout (location = 0) in vec3 aPos;
+	layout (location = 1) in vec2 aTexCoord;
+
+	out vec2 TexCoord;	
+
 	void main()
 	{
 		gl_Position = vec4(aPos, 1.0f);
-	}
-)";
+		TexCoord = aTexCoord;
+	})";
 
 	static const char* fragmentShaderSource = R"(#version 330 core
+	in vec2 TexCoord;
+
 	out vec4 FragColor;
+
+	uniform sampler2D uTexture;
+
 	void main()
 	{
-		FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);
-	}
-)";
-
+		FragColor = texture(uTexture, TexCoord);
+	})";
 };
